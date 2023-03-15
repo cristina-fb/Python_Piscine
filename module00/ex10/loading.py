05f27f163d5e57cfff8c1d471dc31fe6d81ee5e0
Cristina Fernandez baldovinos <cristina_fb_26@hotmail.es>
2023-03-15 12:43:37 +0100

    Progress bar is working

--- module00/ex10/loading.py
+++ module00/ex10/loading.py
@@ -1,25 +1,26 @@
-import time
+import time, sys
 
 def ft_progress(lst):
     eta = 0
-    i = 0
-    max_space = 20
+    i=0
+    max_space = 19
     length = len(lst)
     init_time = time.time()
     dig = len(str(length))
     for x in lst:
         i += 1
-        percent = (i/length) * 100
-        equals = int((percent * max_space) / 100) - 1
-        arrow = (equals * '=') + '>' + (max_space - equals) * ' '
+        percent = int((i/length) * 100)
+        equals = int((percent * max_space) / 100)
+        arrow = ((equals) * '=') + '>' + (max_space - equals) * ' '
         et = time.time() - init_time
-        print("ETA: "+ '{:.2f}'.format(eta) + "s [ " + '{:3d}'.format(percent) + " %][" + arrow + "] " + str(i) + "/" + str(length) + " | elapsed time " + '{:.2f}'.format(et) + "s")
-        yield
+        eta = (et * length) / i
+        print("ETA: "+ '{:.2f}'.format(eta) + "s [ " + '{:3d}'.format(percent) + " %][" + arrow + "] " + "{:{}}".format(str(i), dig) + "/" + str(length) + " | elapsed time " + '{:.2f}'.format(et) + "s", end = '\r')
+        yield i
 
-listy = range(1000)
+listy = range(3333)
 ret = 0
 for elem in ft_progress(listy):
-    #ret += (elem + 3) % 5
-    time.sleep(0.01)
+    ret += elem
+    time.sleep(0.005)
 print()
 print(ret)
