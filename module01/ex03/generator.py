d0924ea5aeb99ed78a14c07b56361100e4792c53
Cristina Fernandez baldovinos <cristina_fb_26@hotmail.es>
2023-03-17 15:45:09 +0100

    module01 ex03 done

--- module01/ex03/generator.py
+++ module01/ex03/generator.py
@@ -1,24 +1,50 @@
-import sys
+import sys, random
 
 def generator(text, sep=" ", option=None):
     """"Divide el texto de acuerdo al valor de sep y producirá las sub-strings. option especifica si una acción se realizará sobre las sub-strings antes de ser producidas."""
-    if type(text) != str:
-        sys.exit("ERROR!")
+    if (type(text) != str) or (type(sep) != str):
+        print("ERROR!")
+        return
     aux = text.split(sep)
-    if type(text) != str:
-        sys.exit("ERROR!")
+    if (type(option) != str) and (option != None):
+        print("ERROR!")
+        return
     if option == "ordered":
-        aux = aux.sort()
-        # print(aux)
-        # for i in aux:
-        #     print(i)
-    if option == "unique":
+        aux.sort()
+        for i in aux:
+            print(i)
+    elif option == "unique":
         aux = list(set(aux))
         for i in aux:
             print(i)
-    # if option == "shuffle":
+    elif option == "shuffle":
+        lst = []
+        for i in range(len(aux)):
+            r = random.randint(0, len(aux) - 1)
+            while r in lst:
+                r = random.randint(0, len(aux) - 1)
+            lst.append(r)
+            print(aux[r])
+    elif option == None:
+        for i in aux:
+            print(i)
     else:
-        sys.exit("ERROR!")
+        print("ERROR")
+        return
 
-
-generator("Hola/caracola/Hola", '/', 'unique')
+text = "Le Lorem Ipsum est simplement du faux texte."
+generator(text, sep=" ")
+print()
+generator(text, sep=" ", option="shuffle")
+print()
+generator(text, sep=" ", option="ordered")
+print()
+generator(text, sep=" ", option="unique")
+print()
+generator(1.0, sep=" ", option="unique")
+print()
+generator(text, sep=2, option="unique")
+print()
+generator(text, sep=" ", option="uniqueee")
+print()
+generator(1.0, sep=" ", option=34.7)
